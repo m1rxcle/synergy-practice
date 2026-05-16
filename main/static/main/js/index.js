933ae0d48798b3935932d78a33b4455ad6494b1a
Denis <noonebesideu@gmail.com>
2026-05-16 20:47:54 +0200

    Изменил Readme. Убрал лишний импорт во views. В js сделал обработку на undefind

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -1,8 +1,10 @@
 const input = document.getElementById("input")
 
-input.addEventListener("input", () => {
-	setTimeout(() => {
-		input.classList.remove("form-input-bounce")
-	}, 100)
-	input.classList.add("form-input-bounce")
-})
+if (input) {
+	input.addEventListener("input", () => {
+		setTimeout(() => {
+			input.classList.remove("form-input-bounce")
+		}, 100)
+		input.classList.add("form-input-bounce")
+	})
+}
--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Django
 from .forms import User_Form
 from urllib.parse import urlencode
